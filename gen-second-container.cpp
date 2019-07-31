c11910333eaaffcd8bbb8d9299fe4439fd60fd05
Xiaoming Mo <mo_xiao_ming@yahoo.com>
2019-07-31 22:18:56 +0800

    remove_copy remove_copy_if

--- gen-second-container.cpp
+++ gen-second-container.cpp
@@ -6,13 +6,39 @@
 
 #include "my_algorithm.h"
 
-TEST_CASE("017 copy")
+TEST_CASE("029 remove_cpy")
 {
     constexpr auto arr = std::array{0, 1, 2, 3, 4, 5};
-    auto dest = std::vector<int>{};
+    constexpr auto result = std::array{0, 1, 3, 4, 5};
 
     SECTION("stl")
     {
+        auto dest = std::vector<int>{};
+        std::remove_copy(begin(arr), end(arr), std::back_inserter(dest), 2);
+        CHECK(std::equal(begin(dest), end(dest), begin(result), end(result)));
+    }
+}
+
+TEST_CASE("030 remove_copy_if")
+{
+    constexpr auto arr = std::array{0, 1, 2, 3, 4, 5};
+    constexpr auto result = std::array{1, 3, 5};
+
+    SECTION("stl")
+    {
+        auto dest = std::vector<int>{};
+        std::remove_copy_if(begin(arr), end(arr), std::back_inserter(dest), is_even<int>);
+        CHECK(std::equal(begin(dest), end(dest), begin(result), end(result)));
+    }
+}
+
+TEST_CASE("017 copy")
+{
+    constexpr auto arr = std::array{0, 1, 2, 3, 4, 5};
+
+    SECTION("stl")
+    {
+        auto dest = std::vector<int>{};
         std::copy(begin(arr), end(arr), std::back_inserter(dest));
         CHECK(std::equal(begin(arr), end(arr), begin(dest), end(dest)));
     }
@@ -21,12 +47,12 @@ TEST_CASE("017 copy")
 TEST_CASE("018 copy_if")
 {
     constexpr auto arr = std::array{0, 1, 2, 3, 4, 5};
-    auto dest = std::vector<int>{};
+    constexpr auto result = std::array{0, 2, 4};
 
     SECTION("stl")
     {
+        auto dest = std::vector<int>{};
         std::copy_if(begin(arr), end(arr), std::back_inserter(dest), is_even<int>);
-        constexpr auto result = std::array{0, 2, 4};
         CHECK(std::equal(begin(dest), end(dest), begin(result), end(result)));
     }
 }
@@ -34,12 +60,12 @@ TEST_CASE("018 copy_if")
 TEST_CASE("019 copy_n")
 {
     constexpr auto arr = std::array{0, 1, 2, 3, 4, 5};
-    auto dest = std::vector<int>{};
+    constexpr auto result = std::array{0, 1, 2};
 
     SECTION("stl")
     {
+        auto dest = std::vector<int>{};
         std::copy_n(begin(arr), 3, std::back_inserter(dest));
-        constexpr auto result = std::array{0, 1, 2};
         CHECK(std::equal(begin(dest), end(dest), begin(result), end(result)));
     }
 }
@@ -47,10 +73,10 @@ TEST_CASE("019 copy_n")
 TEST_CASE("020 copy_backward")
 {
     constexpr auto arr = std::array{0, 1, 2, 3, 4, 5};
-    auto dest = std::vector<int>(arr.size());
 
     SECTION("stl")
     {
+        auto dest = std::vector<int>(arr.size());
         std::copy_backward(begin(arr), end(arr), end(dest));
         CHECK(std::equal(begin(arr), end(arr), begin(dest), end(dest)));
     }
@@ -59,10 +85,10 @@ TEST_CASE("020 copy_backward")
 TEST_CASE("021 move")
 {
     constexpr auto arr = std::array{0, 1, 2, 3, 4, 5};
-    auto dest = std::vector<int>{};
 
     SECTION("stl")
     {
+        auto dest = std::vector<int>{};
         std::move(begin(arr), end(arr), std::back_inserter(dest));
         CHECK(std::equal(begin(arr), end(arr), begin(dest), end(dest)));
     }
@@ -71,10 +97,10 @@ TEST_CASE("021 move")
 TEST_CASE("022 move_backward")
 {
     constexpr auto arr = std::array{0, 1, 2, 3, 4, 5};
-    auto dest = std::vector<int>(arr.size());
 
     SECTION("stl")
     {
+        auto dest = std::vector<int>(arr.size());
         std::move_backward(begin(arr), end(arr), end(dest));
         CHECK(std::equal(begin(arr), end(arr), begin(dest), end(dest)));
     }
