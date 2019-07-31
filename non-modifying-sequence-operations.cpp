e79283035bc2ab5ed64760b5b35d53a87974fe2e
Xiaoming Mo <mo_xiao_ming@yahoo.com>
2019-07-31 19:08:33 +0800

    adjacent_find

--- non-modifying-sequence-operations.cpp
+++ non-modifying-sequence-operations.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <algorithm>
 #include <array>
 
@@ -304,5 +305,23 @@ TEST_CASE("013 find_first_of with func")
         CHECK(std::find_first_of(begin(arr), end(arr), begin(pat), end(pat),
                                  [](auto i, auto j) { return i + 1 == j; })
               == (begin(arr) + 1));
+    }
+}
+
+TEST_CASE("014 adjacent_find")
+{
+    constexpr auto arr = std::array{0, 1, 1, 2, 2};
+
+    SECTION("stl") { CHECK(std::adjacent_find(begin(arr), end(arr)) == (begin(arr) + 1)); }
+}
+
+TEST_CASE("014 adjacent_find with func")
+{
+    constexpr auto arr = std::array{0, 1, 1, 2, 2, 4, 5};
+
+    SECTION("stl")
+    {
+        CHECK(std::adjacent_find(begin(arr), end(arr), [](auto i, auto j) { return j - i == 2; })
+              == (begin(arr) + 4));
     }
 }
